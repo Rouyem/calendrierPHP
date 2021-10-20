44784d73a0467ebb1d24c46e908550cf8a55cb01
emilie <rouyemilie82@gmail.com>
2021-10-20 10:15:41 +0200

    formulaire d'ajout d'évènement

---
+++ public/add.php
@@ -0,0 +1,49 @@
+<?php
+require '../src/bootstrap.php';
+render('header', ['title' => 'Ajouter un événement']);
+?>
+
+<div class="container">
+    <h1>Ajouter un événement</h1>
+    <form action="" method="POST" class="form">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="name">Titre</label>
+                    <input id="name" type="text" required class="form-control" name="name">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="date">Date</label>
+                    <input id="date" type="date" required class="form-control" name="date">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="start">Démarrage</label>
+                    <input id="start" type="time" required class="form-control" name="start" placeholder="HH:MM">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="end">Fin</label>
+                    <input id="end" type="time" required class="form-control" name="end" placeholder="HH:MM">
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary">Ajouter l'évènement</button>
+        </div>
+    </form>
+</div>
+
+<?php
+render('footer');
+?>
