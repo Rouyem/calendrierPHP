0c99be55b7922eecdc11375220af0e7c7864895d
emilie <rouyemilie82@gmail.com>
2021-10-20 09:49:02 +0200

    button

--- public/index.php
+++ public/index.php
@@ -21,36 +21,40 @@ $events = $events->getEventsBetweenByDay($start, $end);
 require '../views/header.php';
 ?>
 
-<div class="d-flex flex-row align-items-center justify-content-between mx-sm-3">
-    <h1><?= $month->toString() ?></h1>
-    <div>
-        <a href="/index.php?month=<?= $month->previousMonth()->month; ?>&year=<?= $month->previousMonth()->year; ?>" class="btn btn-primary">&lt;</a>
-        <a href="/index.php?month=<?= $month->nextMonth()->month; ?>&year=<?= $month->nextMonth()->year; ?>" class="btn btn-primary">&gt;</a>
+<div class="calendar">
+    <div class="d-flex flex-row align-items-center justify-content-between mx-sm-3">
+        <h1><?= $month->toString() ?></h1>
+        <div>
+            <a href="/index.php?month=<?= $month->previousMonth()->month; ?>&year=<?= $month->previousMonth()->year; ?>" class="btn btn-primary">&lt;</a>
+            <a href="/index.php?month=<?= $month->nextMonth()->month; ?>&year=<?= $month->nextMonth()->year; ?>" class="btn btn-primary">&gt;</a>
+        </div>
     </div>
+    <table class="calendar__table calendar__table--<?= $weeks; ?>weeks">
+        <?php for ($i = 0; $i < $weeks; $i++) : ?>
+            <tr>
+                <?php foreach ($month->days as $k => $day) :
+                    $date = (clone $start)->modify("+" . ($k + $i * 7) . "days");
+                    $eventsForDay = $events[$date->format('Y-m-d')] ?? []; ?>
+                    <td class="<?= $month->withinMonth($date) ? '' : 'calendar__othermonth'; ?>">
+                        <?php if ($i === 0) : ?>
+                            <div class="calendar__weekday"><?= $day; ?></div>
+                        <?php endif; ?>
+                        <div class="calendar__day"><?= $date->format('d'); ?></div>
+                        <?php foreach ($eventsForDay as $event) : ?>
+                            <div class="calendar__event">
+                                <?= (new DateTime($event['start']))->format('H:i'); ?> - <a href="/event.php?id=<?= $event['id']; ?>"><?= $event['name']; ?></a>
+
+                            </div>
+                        <?php endforeach; ?>
+                    </td>
+                <?php endforeach; ?>
+            </tr>;
+        <?php endfor; ?>
+    </table>
+
+    <a href="/add.php" class="calendar__button">+</a>
+
 </div>
-<table class="calendar__table calendar__table--<?= $weeks; ?>weeks">
-    <?php for ($i = 0; $i < $weeks; $i++) : ?>
-        <tr>
-            <?php foreach ($month->days as $k => $day) :
-                $date = (clone $start)->modify("+" . ($k + $i * 7) . "days");
-                $eventsForDay = $events[$date->format('Y-m-d')] ?? []; ?>
-                <td class="<?= $month->withinMonth($date) ? '' : 'calendar__othermonth'; ?>">
-                    <?php if ($i === 0) : ?>
-                        <div class="calendar__weekday"><?= $day; ?></div>
-                    <?php endif; ?>
-                    <div class="calendar__day"><?= $date->format('d'); ?></div>
-                    <?php foreach ($eventsForDay as $event) : ?>
-                        <div class="calendar__event">
-                            <?= (new DateTime($event['start']))->format('H:i'); ?> - <a href="/event.php?id=<?= $event['id']; ?>"><?= $event['name']; ?></a>
-
-                        </div>
-                    <?php endforeach; ?>
-                </td>
-            <?php endforeach; ?>
-        </tr>;
-    <?php endfor; ?>
-</table>
-
 <?php
 require '../views/footer.php';
 ?>
